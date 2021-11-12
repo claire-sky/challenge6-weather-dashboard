fdbaf564d24e2baeb6e9c854a0dc77e86b454d52
Claire Sky <eclconnolly@gmail.com>
2021-11-12 12:19:30 -0800

    location and weather apis

---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+var cityName = document.querySelector("#city");
+var fetchButton = document.getElementById("search-btn");
+var city = [];
+var lat = 0;
+var lon = 0;
+var cityHistory = [];
+
+// retrieve lat and lon
+var fetchCity = function(event) {
+    event.preventDefault();
+
+    // retrieve city name and alert when empty
+    city = cityName.value.trim();
+    if(!city) {
+        alert("Please enter a city");
+    };
+
+    // api call for location
+    var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=2e12e8363bb5cb74188b7d051abc37da";
+
+    // fetching api and checking response
+    fetch(locationUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                if (data.length === 0) {
+                    alert("Please enter a valid city");
+                } else {
+                    console.log(city);
+                    console.log(data);
+                    lat = data[0].lat;
+                    lon = data[0].lon;
+                    console.log(lat, lon);
+                    saveCity(city);
+                    fetchWeather(city);
+                }
+            })
+        } else {
+            alert("Please enter a valid city");
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to retrieve weather currently.");
+    });
+};
+
+// retrieve current and 5 day weather
+var fetchWeather = function(city) {
+    console.log(lat, lon);
+
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=2e12e8363bb5cb74188b7d051abc37da";
+
+    fetch(weatherUrl).then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    })
+};
+
+// save city search to local storage
+var saveCity = function(city) {
+    loadCities();
+    for (var i = 0; i < cityHistory.length; i++) {
+        if (cityHistory[i].city === city) {
+            cityHistory.splice([i], 1);
+            break;
+        };
+    };
+    if (cityHistory.length > 6) {
+        cityHistory.shift();
+    };
+    cityHistory.push({"city": city});
+    localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+};
+
+// retrieve city from local storage
+var loadCities = function() {
+    cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+    if (!cityHistory) {
+        cityHistory = [];
+    };
+};
+
+// display cities on aside
+var displayCities = function() {
+    loadCities();
+
+    // loop through cities and add to aside
+    for (var i = 0; i < cityHistory.length; i++) {
+        var btn = document.createElement("button");
+        btn.type = "submit";
+        btn.name = "formBtn";
+        btn.classList.add("btn", "btn-primary", "mx-auto", "m-2", "cityBtn");
+        btn.innerHTML = cityHistory[i].city;
+
+        // add each to aside
+        document.getElementById("search-history").appendChild(btn);
+        // load weather when button is clicked
+        btn.addEventListener("click", fetchCity);
+    };
+};
+
+fetchButton.addEventListener("click", fetchCity);
+displayCities();
