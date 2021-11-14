0aceef409e96f4c002b04f50551018186e96f833
Claire Sky <eclconnolly@gmail.com>
2021-11-13 17:37:07 -0800

    css and bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ var city = [];
 var lat = 0;
 var lon = 0;
 var cityHistory = [];
+var currentWeather = document.getElementById("current-weather");
+var fiveDayWeather = document.getElementById("five-day-weather");
+var searchHistory = document.getElementById("search-history");
 
 // update city value when city is typed and submitted
 var cityInput = function(event) {
@@ -15,12 +18,10 @@ var cityInput = function(event) {
         alert("Please enter a city");
     };
     fetchCity();
-
 };
 
 // retrieve lat and lon
 var fetchCity = function() {
-    
     // api call for location
     var locationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=2e12e8363bb5cb74188b7d051abc37da";
 
@@ -31,15 +32,12 @@ var fetchCity = function() {
                 if (data.length === 0) {
                     alert("Please enter a valid city");
                 } else {
-                    console.log(city);
-                    console.log(data);
                     lat = data[0].lat;
                     lon = data[0].lon;
-                    console.log(lat, lon);
-                    saveCity();
-                    fetchWeather();
+                    saveCity(city);
+                    fetchWeather(city);
                 }
-            })
+            });
         } else {
             alert("Please enter a valid city");
         }
@@ -51,67 +49,73 @@ var fetchCity = function() {
 
 // retrieve current and 5 day weather
 var fetchWeather = function() {
-    console.log(lat, lon);
-
     var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=2e12e8363bb5cb74188b7d051abc37da";
 
     fetch(weatherUrl).then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data);
+        while (currentWeather.hasChildNodes()) {  
+            currentWeather.removeChild(currentWeather.firstChild);
+        };
+        while (fiveDayWeather.hasChildNodes()) {  
+            fiveDayWeather.removeChild(fiveDayWeather.firstChild);
+        }
+
         // convert UTC date to PST date
         var dt = new Date(data.current.dt * 1000);
         var returnDate = dt.toLocaleString().split(",");
         returnDate = returnDate[0];
 
         // update main with current city and date
-        var currentWeather = document.createElement("h2");
-        currentWeather.innerHTML = city + " (" + returnDate + ")";
-        document.getElementById("current-weather").appendChild(currentWeather);
+        var currentWeatherDate = document.createElement("h2");
+        currentWeatherDate.innerHTML = city + " (" + returnDate + ")";
 
         // update main with current temp
         var currentTemp = document.createElement("p");
         currentTemp.innerHTML = "Temp: " + data.current.temp + " °F";
-        document.getElementById("current-weather").appendChild(currentTemp);
 
         // update main with current wind
         var currentWind = document.createElement("p");
         currentWind.innerHTML = "Wind: " + data.current.wind_speed + " MPH";
-        document.getElementById("current-weather").appendChild(currentWind);
         
         // update main with current humidity
         var currentHumidity = document.createElement("p");
         currentHumidity.innerHTML = "Humidity: " + data.current.humidity + " %";
-        document.getElementById("current-weather").appendChild(currentHumidity);
         
         // update main with current UV index
         var currentUVIndex = document.createElement("p");
         currentUVIndex.innerHTML = "UV Index: " + data.current.uvi;
-        document.getElementById("current-weather").appendChild(currentUVIndex);
+        currentWeather.append(currentWeatherDate, currentTemp, currentWind, currentHumidity, currentUVIndex);
 
-        for (var i = 0; i < 5; i++) {
-
+        for (var i = 1; i < 6; i++) {
             // update main with 5-day dividers for each day
             var fiveDayDiv = document.createElement("div");
             fiveDayDiv.setAttribute("id", "five-day-div" + [i])
-            fiveDayDiv.classList.add("col-2", "bg-primary", "text-light")
-            document.getElementById("five-day-weather").appendChild(fiveDayDiv);
+            fiveDayDiv.classList.add("col-2", "bg-primary", "text-light", "m-2", "rounded")
+            fiveDayWeather.appendChild(fiveDayDiv);
+
+            // convert UTC date to PST date
+            var dtDaily = new Date(data.daily[i].dt * 1000);
+            var returnDateDaily = dtDaily.toLocaleString().split(",");
+            returnDateDaily = returnDateDaily[0];
+
+            // update 5-day dividers with date
+            var fiveDayDay = document.createElement("p");
+            fiveDayDay.innerHTML = returnDateDaily;
 
             // update 5-day dividers with temp
             var fiveDayTemp = document.createElement("p");
             fiveDayTemp.innerHTML = "Temp: " + data.daily[i].temp.day + " °F";
-            document.getElementById("five-day-div" + [i]).appendChild(fiveDayTemp);
 
             // update 5-day dividers with wind
             var fiveDayWind = document.createElement("p");
             fiveDayWind.innerHTML = "Temp Max: " + data.daily[i].wind_speed + " MPH";
-            document.getElementById("five-day-div" + [i]).appendChild(fiveDayWind);
 
             // update 5-day dividers with humidity
             var fiveDayHumidity = document.createElement("p");
             fiveDayHumidity.innerHTML = "Temp Max: " + data.daily[i].humidity + " %";
-            document.getElementById("five-day-div" + [i]).appendChild(fiveDayHumidity);
+            document.getElementById("five-day-div" + [i]).append(fiveDayDay, fiveDayTemp, fiveDayWind, fiveDayHumidity);
         };
     });
 };
@@ -119,17 +123,24 @@ var fetchWeather = function() {
 // save city search to local storage
 var saveCity = function(city) {
     loadCities();
+
+    // check city history for entered city and remove from history if match
     for (var i = 0; i < cityHistory.length; i++) {
         if (cityHistory[i].city === city) {
             cityHistory.splice([i], 1);
             break;
         };
     };
+
+    // limit saved cities to 6
     if (cityHistory.length > 6) {
         cityHistory.shift();
     };
+
+    // add new city to history
     cityHistory.push({"city": city});
     localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+    displayCities();
 };
 
 // retrieve city from local storage
@@ -143,18 +154,21 @@ var loadCities = function() {
 // display cities on aside
 var displayCities = function() {
     loadCities();
+    while (searchHistory.hasChildNodes()) {  
+        searchHistory.removeChild(searchHistory.firstChild);
+    };
 
     // loop through cities and add to aside
     for (var i = 0; i < cityHistory.length; i++) {
         var btn = document.createElement("button");
-        btn.type = "submit";
+        btn.type = "button";
         btn.name = "formBtn";
-        btn.classList.add("btn", "btn-primary", "mx-auto", "m-2", "cityBtn");
+        btn.classList.add("btn", "btn-primary", "mx-auto", "m-2", "cityBtn", "d-grid","gap-2", "col");
         btn.innerHTML = cityHistory[i].city;
         btn.value = cityHistory[i].city;
 
         // add each to aside
-        document.getElementById("search-history").appendChild(btn);
+        searchHistory.appendChild(btn);
         // load weather when button is clicked
         btn.addEventListener("click", cityClick);
     };
